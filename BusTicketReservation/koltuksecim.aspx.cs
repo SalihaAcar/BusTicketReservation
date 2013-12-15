c017cdb91abfbab7d1d0a89d0f42730b20fe2b45
SalihaAcar <sacar@ogu.edu.tr>
2013-12-15 16:40:09 +0200

    Some smal additions to third page
    koltuksecim.aspx.cs (Connection instructions and page_load event)

--- BusTicketReservation/koltuksecim.aspx.cs
+++ BusTicketReservation/koltuksecim.aspx.cs
@@ -4,13 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using System.Drawing.Imaging;
+
 
 namespace BusTicketReservation
 {
     public partial class koltuksecim : System.Web.UI.Page
     {
+        SqlConnection con = new SqlConnection(@"data source=.\SQLEXPRESS; initial catalog=otobus;integrated security=true;");
+        DataTable table = new DataTable(); // Bir tane tablo tutar.
         protected void Page_Load(object sender, EventArgs e)
         {
+
+            SqlCommand cmd = new SqlCommand("Select * from seferliste where SeferNo=@SeferNo", con);
+            string tmp = Request.QueryString["SeferNo"].ToString();
+            cmd.Parameters.AddWithValue("@SeferNo", Request.QueryString["SeferNo"].ToString());
+            SqlDataAdapter d = new SqlDataAdapter(cmd);
+            d.Fill(table);
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+            
+        
 
         }
     }
